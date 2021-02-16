b62483703bbc6aac56321f614b75c35b26114ed4
YuriCstr <r17.yuridecastro@gmail.com>
2021-02-16 17:36:30 -0300

    Update gradle.properties, home_page.jpg, and 17 more files...

---
+++ lib/blocks/Sblock.dart
@@ -0,0 +1,15 @@
+import 'package:Tetris_Project/blocks/point.dart';
+import 'package:flutter/material.dart';
+import 'block.dart';
+
+class SBlock extends Block {
+  SBlock(int width) {
+    points[0] = Point((width / 2 - 1).floor(), -1);
+    points[1] = Point((width / 2 + 0).floor(), -1);
+    points[2] = Point((width / 2 + 0).floor(), 0);
+    points[3] = Point((width / 2 + 1).floor(), 0);
+
+    rotationCenter = points[1];
+    color = Colors.green;
+  }
+}
